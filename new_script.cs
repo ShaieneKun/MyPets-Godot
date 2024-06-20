78372ac6dad46b8ada110dba7e80911add5d764a
ShaieneKun <rafaelshaiene115@gmail.com>
2024-06-19 23:35:58 -0300

    Manipulating node programmatically

--- new_script.cs
+++ new_script.cs
@@ -4,17 +4,23 @@ using System;
 public partial class new_script : Node
 {
 	private int frameCount = 0;
+	private Label _label;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Godot.GD.Print("Hello, World!");
+		_label = GetNode<Label>("Camera2D/Label");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		this.frameCountIncrement();
-		Godot.GD.Print("Current frame: " + getFrameCount());
+
+		string currentFrame = "Current frame: " + getFrameCount();
+		Godot.GD.Print(currentFrame);
+
+		_label.Text = "Hello World!\n" + currentFrame;
 	}
 
 	private int getFrameCount()

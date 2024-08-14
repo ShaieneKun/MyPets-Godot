ba0bab9b4984d64b4bb3edbef05cc13324702cef
ShaieneKun <rafaelshaiene115@gmail.com>
2024-08-13 23:24:42 -0300

    WIP: Pet movement

--- MyPets/src/Pet.cs
+++ MyPets/src/Pet.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Pet : RigidBody2D
 {
+	[Export]
+	public int Speed { get; set; } = 15; // How fast the player will move (pixels/sec).
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,19 +15,27 @@ public partial class Pet : RigidBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		_PetMovement(delta);
 	}
 
-	private void _PetMovement()
+	private void _PetMovement(double delta)
 	{
-		AnimatedSprite2D animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D2");
-		string[] petTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+		// AnimatedSprite2D pet = GetNode<AnimatedSprite2D>("AnimatedSprite2D2");
+		int direction = 0;
+		Vector2 velocity = Vector2.Down;
 
-		animatedSprite2D.Play(petTypes[GD.Randi() % petTypes.Length]);
+		Position += velocity * Speed * (float)delta;
+
+		// string[] petTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+
+		// animatedSprite2D.Play(petTypes[GD.Randi() % petTypes.Length]);
+		// pet.Position
 	}
 
 	// Signals
 	private void OnVisibleOnScreenNotifier2DScreenExited()
 	{
+		GD.Print("Out of screen");
 		QueueFree();
 	}
 }
